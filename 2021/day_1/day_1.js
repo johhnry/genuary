63568170f652823a95287bfc70c8cb7d0661c37d
Joseph Henry <josephenry@protonmail.com>
2021-01-05 23:42:44 +0100

    day 1 and readme

---
+++ 2021/day_1/day_1.js
@@ -0,0 +1,89 @@
+// (c) 2021 Joseph HENRY
+// This code is licensed under MIT license (see LICENSE for details)
+
+// The number of circles
+const nCircles = 15;
+
+// The radius of the biggest circle
+const firstRadius = 150;
+
+let offset = 0;
+
+/**
+ * Give the angle on a circle at that index
+ */
+function angleFromIndex(index, steps) {
+  return index * (TWO_PI / steps);
+}
+
+/**
+ * Translate on a circle at given angle
+ */
+function translateOnCircle(angle, radius) {
+  translate(cos(angle) * radius, sin(angle) * radius);
+}
+
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+
+  // Go to the center
+  translate(width / 2, height / 2);
+
+  // Display first circle
+  noFill();
+  stroke(255);
+  circle(0, 0, firstRadius * 2);
+
+  let angle;
+
+  // First loop
+  for (let i = 0; i < nCircles; i++) {
+    push();
+
+    // Compute angle and radius
+    angle = angleFromIndex(i, nCircles);
+    const secondRadius = cos(angle + offset) * firstRadius / 2;
+
+    // Move to the circle
+    translateOnCircle(angle, firstRadius);
+
+    // Draw it
+    circle(0, 0, secondRadius * 2);
+
+    // Second loop
+    for (let j = 0; j < nCircles; j++) {
+      push();
+
+      angle = angleFromIndex(j, nCircles);
+      const thirdRadius = sin(angle - offset * 2) * secondRadius / 4;
+
+      translateOnCircle(angleFromIndex(j, nCircles), secondRadius);
+      circle(0, 0, thirdRadius * 2);
+
+      // Third nested loop!
+      for (let k = 0; k < nCircles / 2; k++) {
+        push();
+
+        angle = angleFromIndex(k, nCircles);
+        const fourthRadius = cos(angle - offset * 4) * thirdRadius / 2;
+
+        translateOnCircle(angleFromIndex(k, nCircles / 2), thirdRadius);
+        circle(0, 0, fourthRadius * 2);
+
+        pop();
+      }
+
+      pop();
+    }
+
+    pop();
+  }
+
+  // Increase the offset
+  offset += 0.01;
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Genuary
+
+This repository stores the programs made during the [genuary](https://genuary2021.github.io/) event.
+
+> GENUARY is an artificially generated month of time where we build code that makes beautiful things.
+>
+
+You can click on any animation below to open the source file, enjoy!
+
+## 2021 edition
+
+| [![](./2021/day_1/genuary_1.gif)](./2021/day_1/day_1.js)<br><center>TRIPLE NESTED LOOP</center> |      |      |
+| ------------------------------------------------------------ | ---- | ---- |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+|                                                              |      |      |
+
+
+
+## Links
+
+- [genuary home page](https://genuary2021.github.io)
+
+- [p5js website](p5js.org/)
+
+- [p5js web editor](https://editor.p5js.org/)
+
+  
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
